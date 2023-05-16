04cccb4ebd127107a02b66f10bf8743435f8f03b
afifmansor <132339479+afifmansor@users.noreply.github.com>
2023-05-16 13:48:55 +0800

    Update generator-try-1.py

--- generator-try-1.py
+++ generator-try-1.py
@@ -1,4 +1,8 @@
 import streamlit as st
 
-st.write("Hello world!")
+# Define a Unicode string with the smile emoji
+smile = u"\U0001F600"
+
+# Display the message with the smile emoji
+st.write("Hi world, this is Afif Mansor " + smile)
 
