7fbc3b5c3694a9c923545acb5b7d0b595f9154b0
afifmansor <132339479+afifmansor@users.noreply.github.com>
2023-05-18 09:50:51 +0800

    Update SchemeColorExtactor.py

--- SchemeColorExtactor.py
+++ SchemeColorExtactor.py
@@ -61,17 +61,41 @@ def save_color_schemes_to_csv(df, output_path):
     df.to_csv(output_path, index=False)
 
 
-def main():
-    st.title("Image Color Scheme Generator")
-    st.write("Upload images to generate color schemes.")
-
-    # Allow the user to upload multiple images
-    uploaded_files = st.file_uploader("Upload Images", accept_multiple_files=True)
-
-    if uploaded_files:
+   if uploaded_files:
         st.write("Generating color schemes...")
 
         # Process the uploaded images and generate the color schemes DataFrame
         file_paths = []
         for uploaded_file in uploaded_files:
+            # Save the uploaded file temporarily
+            with open(uploaded_file.name, "wb") as f:
+                f.write(uploaded_file.getbuffer())
+                file_paths.append(uploaded_file.name)
 
+            try:
+                # Extract color scheme from each image
+                colors = extract_color_scheme(uploaded_file.name, num_colors=5)
+
+                # Convert the colors to hexadecimal format
+                hex_colors = ['#%02x%02x%02x' % (color[0], color[1], color[2]) for color in colors]
+
+                # Store the file name and color scheme in a dictionary
+                color_scheme = {'File Name': uploaded_file.name, 'Color Scheme': hex_colors}
+                color_schemes.append(color_scheme)
+            except Exception as e:
+                print(f"Error processing image {uploaded_file.name}: {str(e)}")
+
+        # Create a pandas DataFrame from the color schemes
+        df = pd.DataFrame(color_schemes)
+
+        # Display the color schemes as a table
+        st.write(df)
+
+        # Allow the user to download the color schemes as a CSV file
+        output_path = "color_schemes.csv"
+        save_color_schemes_to_csv(df, output_path)
+        st.download_button("Download Color Schemes CSV", output_path)
+
+        # Remove the temporary uploaded files
+        for file_path in file_paths:
+            os.remove(file_path)
