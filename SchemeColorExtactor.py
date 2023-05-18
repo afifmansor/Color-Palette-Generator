50643dd9dc3237a0c4664869e9e968aac1ef1143
afifmansor <132339479+afifmansor@users.noreply.github.com>
2023-05-18 10:04:35 +0800

    Update csv SchemeColorExtactor.py

--- SchemeColorExtactor.py
+++ SchemeColorExtactor.py
@@ -3,7 +3,6 @@ from PIL import Image
 import pandas as pd
 import numpy as np
 from sklearn.cluster import KMeans
-import os
 
 
 def extract_color_scheme(image_path, num_colors):
@@ -57,7 +56,7 @@ def generate_color_scheme_df(file_paths, num_colors):
 
 
 def save_color_schemes_to_csv(df, output_path):
-    # Save the DataFrame to a CSV file
+    # Save the DataFrame to an Excel file
     df.to_csv(output_path, index=False)
 
 
@@ -69,40 +68,30 @@ def main():
     uploaded_files = st.file_uploader("Upload Images", accept_multiple_files=True)
 
     if uploaded_files:
-       st.write("Generating color schemes...")
+        st.write("Generating color schemes...")
 
-    # Process the uploaded images and generate the color schemes DataFrame
-    file_paths = []
-    for uploaded_file in uploaded_files:
-        # Save the uploaded file temporarily
-        with open(uploaded_file.name, "wb") as f:
-            f.write(uploaded_file.getbuffer())
-            file_paths.append(uploaded_file.name)
+        # Process the uploaded images and generate the color schemes DataFrame
+        file_paths = []
+        for uploaded_file in uploaded_files:
+            # Save the uploaded file temporarily
+            with open(uploaded_file.name, "wb") as f:
+                f.write(uploaded_file.getbuffer())
+                file_paths.append(uploaded_file.name)
 
-        try:
-            # Extract color scheme from each image
-            colors = extract_color_scheme(uploaded_file.name, num_colors=5)
+        df = generate_color_scheme_df(file_paths, num_colors=5)
 
-            # Convert the colors to hexadecimal format
-            hex_colors = ['#%02x%02x%02x' % (color[0], color[1], color[2]) for color in colors]
+        # Display the color schemes as a table
+        st.write(df)
 
-            # Store the file name and color scheme in a dictionary
-            color_scheme = {'File Name': uploaded_file.name, 'Color Scheme': hex_colors}
-            color_schemes.append(color_scheme)
-        except Exception as e:
-            print(f"Error processing image {uploaded_file.name}: {str(e)}")
+        # Allow the user to download the color schemes as an Excel file
+        output_path = "color_schemes.csv"
+        save_color_schemes_to_csv(df, output_path)
+        st.download_button("Download Color Schemes", output_path)
 
-    # Create a pandas DataFrame from the color schemes
-    df = pd.DataFrame(color_schemes)
+        # Remove the temporary uploaded files
+        for file_path in file_paths:
+            os.remove(file_path)
 
-    # Display the color schemes as a table
-    st.write(df)
 
-    # Allow the user to download the color schemes as a CSV file
-    output_path = "color_schemes.csv"
-    save_color_schemes_to_csv(df, output_path)
-    st.download_button("Download Color Schemes CSV", output_path)
-
-    # Remove the temporary uploaded files
-    for file_path in file_paths:
-        os.remove(file_path)
+if __name__ == "__main__":
+    main()
